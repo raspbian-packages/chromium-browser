a6cd05c56af12c931361bd4aca070e166e1c1bda
Debian Chromium Maintainers <pkg-chromium-maint@lists.alioth.debian.org>
2018-11-21 02:17:45 +0000

    openjpeg
    Gbp-Pq: Topic system
    Gbp-Pq: Name openjpeg.patch

--- third_party/pdfium/core/fxcodec/codec/cjpx_decoder.h
+++ third_party/pdfium/core/fxcodec/codec/cjpx_decoder.h
@@ -13,7 +13,7 @@
 #include "core/fxcodec/codec/codec_int.h"
 #include "core/fxcrt/unowned_ptr.h"
 #include "third_party/base/span.h"
-#include "third_party/libopenjpeg20/openjpeg.h"
+#include <openjpeg-2.3/openjpeg.h>
 
 class CPDF_ColorSpace;
 
