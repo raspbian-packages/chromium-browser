0761a5ac7c15078d8b4c1a267e028b5cab158103
Debian Chromium Maintainers <pkg-chromium-maint@lists.alioth.debian.org>
2018-11-21 02:17:45 +0000

    alignof
    Gbp-Pq: Topic fixes
    Gbp-Pq: Name alignof.patch

--- mojo/public/c/system/macros.h
+++ mojo/public/c/system/macros.h
@@ -27,9 +27,7 @@
   (sizeof(void*) == 4 ? 32 : 0)
 
 // Like the C++11 |alignof| operator.
-#if __cplusplus >= 201103L
-#define MOJO_ALIGNOF(type) alignof(type)
-#elif defined(__GNUC__)
+#if defined(__GNUC__)
 #define MOJO_ALIGNOF(type) __alignof__(type)
 #elif defined(_MSC_VER)
 // The use of |sizeof| is to work around a bug in MSVC 2010 (see
